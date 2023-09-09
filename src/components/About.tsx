5884bbbfe98cb5ee90af0cf93cc3bbe31ac4d3cf
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-09-10 00:49:17 +0300

    pda_dj_frontend_build_v_20230909_2344 - dashboard-redir-if-authenticated

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,111 +1,59 @@
-import React from 'react';
-import { Image, Container, Segment, Header, List } from 'semantic-ui-react';
+import React from "react";
+import { Image, Segment, Header, Label } from "semantic-ui-react";
 
-const AboutSection = () => {
+const AboutSection: React.FC = () => {
+  const skillsets = [
+    "Django",
+    "Django Restframework",
+    "React/Next",
+    "TypeScript",
+    "PostgreSQL",
+    "REST APIs",
+    "Semantic CSS",
+    "Semantic UI",
+    "Tailwind CSS",
+    "SQL/No-SQL Databases",
+  ];
+
   return (
-    <Segment inverted vertical style={{ padding: '5em 0em' }}>
-      <Container>
-        <Header as="h3" content="About Us" />
-        <p>
-          Our project was born from a shared passion for personal budgeting
-          and financial management. We believe that managing your finances
-          should be convenient and accessible to everyone. This project is a
-          result of our dedication to providing a user-friendly budgeting
-          system.
-        </p>
-        <p>
-          <strong>Team Members:</strong>
-        </p>
-        <List horizontal>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://www.linkedin.com/in/team-member1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </List.Header>
-              <List.Description>Team Member 1</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://github.com/team-member1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>
-              </List.Header>
-              <List.Description>@team-member1</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://twitter.com/team_member1"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Twitter
-                </a>
-              </List.Header>
-              <List.Description>@team_member1</List.Description>
-            </List.Content>
-          </List.Item>
-        </List>
-        <List horizontal>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://www.linkedin.com/in/team-member2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LinkedIn
-                </a>
-              </List.Header>
-              <List.Description>Team Member 2</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://github.com/team-member2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  GitHub
-                </a>
-              </List.Header>
-              <List.Description>@team-member2</List.Description>
-            </List.Content>
-          </List.Item>
-          <List.Item>
-            <List.Content>
-              <List.Header>
-                <a
-                  href="https://twitter.com/team_member2"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Twitter
-                </a>
-              </List.Header>
-              <List.Description>@team_member2</List.Description>
-            </List.Content>
-          </List.Item>
-        </List>
-        {/* Add more team members as needed */}
-      </Container>
+    <Segment vertical style={{ padding: "4em 0" }}>
+      <Header as="h2" textAlign="center">
+        About the Software Engineer
+      </Header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Image
+          src={require("../assets/images/se_photo.jpeg")}
+          circular
+          size="medium"
+        />
+      </div>
+      <Header as="h3" textAlign="center">
+        Tonny Kirwa
+        </Header>
+      <Header as="h4" textAlign="center">
+        Software Engineer
+      </Header>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Label.Group>
+          {skillsets.map((skill, index) => (
+            <Label key={index} color="green">
+              {skill}
+            </Label>
+          ))}
+        </Label.Group>
+      </div>
     </Segment>
   );
 };
--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,46 +1,46 @@
-import React from 'react';
-import { Image } from 'semantic-ui-react';
-import Footer from './Footer';
-import AboutSection from './About';
+import React from "react";
+import { Image } from "semantic-ui-react";
+import Footer from "./Footer";
+import AboutSection from "./About";
 // import { Header } from 'semantic-ui-react';
 
-const LandingPage = () => (
-  <>
-      <div style={{ position: 'relative' }}>
-    <Image
-      src='https://img.freepik.com/free-photo/top-view-budget-written-note-notepad-with-pen-dark-surface-student-color-school-money-gray-college-copybook_179666-19729.jpg?w=2000'
-      style={{ width: '100%', height: 'auto' }}
-    />
-    <div
-      style={{
-        position: 'absolute',
-        top: '10%',
-        left: '10%',
-        color: 'white',
-        zIndex: 1, // Ensure it's on top of the image
-      }}
-    >
-      <h1>Personal Budgeting with Convenience</h1>
-      <h4>Welcome to Budgeting System</h4>
-    </div>
-    <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'black',
-        opacity: 0.75,
-        zIndex: 0, // Place the layer behind the content
-      }}
-    />
-  </div>
+const LandingPage: React.FC = () => {
+  return (
+    <>
+      <div style={{ position: "relative" }}>
+        <Image
+          src="https://img.freepik.com/free-photo/top-view-budget-written-note-notepad-with-pen-dark-surface-student-color-school-money-gray-college-copybook_179666-19729.jpg?w=2000"
+          style={{ width: "100%", height: "auto" }}
+        />
+        <div
+          style={{
+            position: "absolute",
+            top: "10%",
+            left: "10%",
+            color: "white",
+            zIndex: 1, // Ensure it's on top of the image
+          }}
+        >
+          <h1>Personal Budgeting with Convenience</h1>
+          <h4>Welcome to Budgeting System</h4>
+        </div>
+        <div
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "black",
+            opacity: 0.75,
+            zIndex: 0, // Place the layer behind the content
+          }}
+        />
+      </div>
 
-    <AboutSection/>
-  <Footer/>
-
-  </>
-);
-
+      <AboutSection />
+      <Footer />
+    </>
+  );
+};
 export default LandingPage;
